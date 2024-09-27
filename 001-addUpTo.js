372c0d985cd2a54c0ae5595813a762b67e37f985
Egormity <kotlaregor9@gmail.com>
2024-09-27 11:32:12 +0700

    initial commit

---
+++ 001-addUpTo.js
@@ -0,0 +1,13 @@
+// function addUpTo1(n) {
+//   let sum = 0;
+//   for (let i = 0; i <= n; i++) {
+//     sum += i;
+//   }
+//   return sum;
+// }
+// console.log(addUpTo1(1000000000));
+
+function addUpTo2(n) {
+  return (n * (n + 1)) / 2;
+}
+console.log(addUpTo2(1000000000));
