8c329db6cb2abdabc88df19de122e4a3935aa0cc
Egormity <kotlaregor9@gmail.com>
2024-10-27 13:11:42 +0700

    doubly linked list done

--- 012-doubly-linked-list.js
+++ 012-doubly-linked-list.js
@@ -32,11 +32,74 @@ class DoublyLinkedList {
     pop() {
         if (this.length === 0) return undefined;
 
-        const oldTail = this.tail;
+        const oldTail = { ...this.tail, prev: null };
         this.tail = { ...this.tail.prev, next: null };
         this.tail.prev.next = null;
         this.length--;
         return oldTail;
+    }
+
+    shift() {
+        if (this.length === 0) return undefined;
+
+        const oldHead = { ...this.head, next: null };
+        this.head = { ...this.head.next, prev: null };
+        this.length--;
+        return oldHead;
+    }
+
+    unshift(value) {
+        const newNode = new Node(value);
+        this.head = { ...newNode, next: this.head ? { ...this.head, prev: newNode } : null };
+        this.length++;
+        return this;
+    }
+
+    get(index) {
+        if (index < 0 || index > this.length - 1) return undefined;
+        if (index === 0) return this.head;
+        if (index === this.length - 1) return this.tail;
+
+        let node;
+        if (index < this.length / 2) {
+            node = this.head;
+            for (let i = 0; i < index; i++) {
+                node = node.next;
+            }
+        } else {
+            node = this.tail;
+            for (let i = this.length - 1; i > index; i--) {
+                node = node.prev;
+            }
+        }
+        return node;
+    }
+
+    set(index, value) {
+        const node = this.get(index);
+        return !!(node ? (node.value = value) : false);
+    }
+
+    insert(index, value) {
+        if (index < 0 || index > this.length) return false;
+        if (index === 0) return !!this.unshift(value);
+        if (index === this.length) return !!this.push(value);
+
+        const newNode = new Node(value);
+        this.get(index - 1).next = { ...newNode, next: { ...this.get(index), prev: newNode } };
+        this.length++;
+        return true;
+    }
+
+    remove(index) {
+        if (index < 0 || index > this.length - 1) return undefined;
+        if (index === 0) return this.shift();
+        if (index === this.length - 1) return this.pop();
+
+        const removed = { ...this.get(index), prev: null, next: null };
+        this.get(index - 1).next = this.get(index + 1);
+        this.length--;
+        return removed;
     }
 }
 
@@ -49,5 +112,8 @@ list.push(233);
 list.push(2323233);
 
 // console.log(list.pop());
+// console.log(list.shift());
 
+// console.log(list.get(list.length - 1));
+list.set(0, 0);
 console.log(list);
